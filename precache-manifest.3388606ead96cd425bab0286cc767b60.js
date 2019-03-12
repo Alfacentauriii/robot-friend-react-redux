859a4c495409e39736542d72d81d94d97d548849
Btazoid <tir-na-nog@hotmail.com>
2019-03-12 16:53:15 +0100

    Updates

---
+++ precache-manifest.3388606ead96cd425bab0286cc767b60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-friend-react-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6cc18b61da48eba676a8",
+    "url": "/robot-friend-react-redux/static/js/runtime~main.ab3d53f1.js"
+  },
+  {
+    "revision": "66ab58bda90a00287ad1",
+    "url": "/robot-friend-react-redux/static/js/main.bf826b4b.chunk.js"
+  },
+  {
+    "revision": "11935a83c20cdeccaef3",
+    "url": "/robot-friend-react-redux/static/js/2.40371ad1.chunk.js"
+  },
+  {
+    "revision": "66ab58bda90a00287ad1",
+    "url": "/robot-friend-react-redux/static/css/main.dd1dcd43.chunk.css"
+  },
+  {
+    "revision": "11935a83c20cdeccaef3",
+    "url": "/robot-friend-react-redux/static/css/2.a57ceec9.chunk.css"
+  },
+  {
+    "revision": "35ba9f670d349ce62d263cea31078bb8",
+    "url": "/robot-friend-react-redux/index.html"
+  }
+];
